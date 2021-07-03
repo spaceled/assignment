40b3f7a41770a1bb592427088c2f1a19b16772da
spaceled <spaceled@gmail.com>
2021-07-03 20:50:25 +0600

    Testing

---
+++ assignment.js
@@ -0,0 +1,12 @@
+//https://github.com/spaceled/assignment
+kilometerToMeter
+
+// budgetCalculator(watch=50, phone=100, laptop=500)
+// totalCost
+
+hotelCost(days)
+1-10=100
+11-20=80
+21-=50
+
+megaFriend
