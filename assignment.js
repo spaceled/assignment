77873c3a46d7d14374dc04159e49a8c523bfda3e
spaceled <spaceled@gmail.com>
2021-07-04 10:31:39 +0600

    Final

--- assignment.js
+++ assignment.js
@@ -2,35 +2,51 @@
 
 // Covert Kilometer to Meter
 function kilometerToMeter(kilometer) {
-    return kilometer * 1000;
+    var result =  kilometer * 1000;
+    if (kilometer < 0) {
+        return 'ATTENTION! Kilometer can not be negative.'
+    }
+    else {
+        return result;
+    }
 }
 
 // Total cost of watch, Phone and Laptop
 function budgetCalculator(watch, phone, laptop) {
     var totalCost = watch * 50 + phone * 100 + laptop * 500;
-    return totalCost;
+    if (watch < 0 || phone < 0 || laptop < 0) {
+        return 'ATTENTION! Please, enter positive value.'
+    }
+    else {
+        return totalCost;
+    }
+    
 }
 
 // Hotel Cost
 function hotelCost(day) {
+    //  This will handle 0 or negative value
+    if (day <= 0) {
+        return "ATTENTION! Day can't be negative or Zero"
+    }
     if (day <= 10) {
-        var firstPart = day * 100;
-        return firstPart;
+        var first10Days = day * 100;
+        return first10Days;
     }
     else if (day <= 20) {
-        var firstPart = 10 * 100;
-        var remaining = day - 10;
-        var secondPart = remaining * 80;
-        cost = firstPart + secondPart;
+        var first10Days = 10 * 100; 
+        var remaining = day - 10; // Total days MINUS first 10 days
+        var second20Days = remaining * 80;
+        cost = first10Days + second20Days;
         return cost;
 
     }
     else {
-        var firstPart = 10 * 100;
-        var secondPart = 10 * 80;
-        var remaining = day - 20;
-        var thirdPart = remaining * 50 + secondPart + firstPart;
-        return thirdPart;
+        var first10Days = 10 * 100;
+        var second20Days = 10 * 80;
+        var remaining = day - 20; // Total days MINUS first 20 days
+        var second20Above = remaining * 50 + second20Days + first10Days;
+        return second20Above;
     }
 }
 
@@ -38,7 +54,7 @@ function hotelCost(day) {
 function megaFriend(friendList) {
     var largest = friendList[0];
     if (friendList.length == 0) {
-        return 'ATTENTION! List is Empty';
+        return 'ATTENTION! List is Empty'; // Handle Empty Array
     }
     for (var i = 0; i < friendList.length; i++) {
         if (largest.length < friendList[i].length) {
