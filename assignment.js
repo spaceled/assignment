585cd5717eb5d19d24c6435802ce2cd7167ba018
spaceled <spaceled@gmail.com>
2021-07-04 08:58:26 +0600

    Half Done

--- assignment.js
+++ assignment.js
@@ -1,12 +1,42 @@
 //https://github.com/spaceled/assignment
-kilometerToMeter
 
-// budgetCalculator(watch=50, phone=100, laptop=500)
-// totalCost
+// Covert Kilometer to Meter
+function kilometerToMeter(kilometer) {
+    return kilometer * 1000;
+}
 
-hotelCost(days)
-1-10=100
-11-20=80
-21-=50
+// Total cost of watch, Phone and Laptop
+function budgetCalculator(watch, phone, laptop) {
+    var totalCost = watch * 50 + phone * 100 + laptop * 500;
+    return totalCost;
+}
 
-megaFriend
+// Hotel Cost
+function hotelCost(day) {
+    if (day <= 10) {
+        var firstPart = day * 100;
+        return firstPart;
+    }
+    else if (day <= 20) {
+        var firstPart = 10 * 100;
+        var remaining = day - 10;
+        var secondPart = remaining * 80;
+        cost = firstPart + secondPart;
+        return cost;
+
+    }
+    else {
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remaining = day - 20;
+        var thirdPart = remaining * 50 + secondPart + firstPart;
+        return thirdPart;
+    }
+}
+// 1-10=100
+// 11-20=80
+// 21-=50
+
+// megaFriend
+var cost = hotelCost(7);
+console.log(cost);
